512b13258023d705f45acc551642f94ae5cf01ee
wedsonxse <wedsonlino27@gmail.com>
2023-10-10 14:41:10 -0300

    feat: adjusted transaction

--- commerce (1)/commerce/src/main/java/com/commerce/commerce/services/TransactionService.java
+++ commerce (1)/commerce/src/main/java/com/commerce/commerce/services/TransactionService.java
@@ -4,6 +4,8 @@ import com.commerce.commerce.adapters.Dispatcher;
 import com.commerce.commerce.dto.TransactionDTO;
 import com.commerce.commerce.entities.User;
 import com.commerce.commerce.entities.UserType;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -12,8 +14,9 @@ import org.springframework.stereotype.Service;
 public class TransactionService {
     private final UserService userService;
     private final Authorizer authorizationGateway;
-
     private final Dispatcher notificationSender;
+    private Session session = null;
+    private Transaction transaction = null;
 
     @Autowired
     public TransactionService(UserService userService, @Qualifier("Auth") Authorizer authorizer, @Qualifier("Notifier") Dispatcher notifier){
@@ -24,30 +27,60 @@ public class TransactionService {
 
     public void performTransaction(TransactionDTO request) throws Exception {
         try{
+            session = session.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
             User payee = this.userService.findById(request.getPayee());
             User payer = this.userService.findById(request.getPayer());
             Double amount = request.getAmount();
 
-            if (payer.getType() == UserType.SHOPKEEPER) {
-                throw new Exception("O pagador não pode ser lojista");
-            }
+            this.validatePayerTypeIsNotCustomer(payer);
+            this.validatePayerHasEnoughCredits(payer,amount);
+            this.authorizeTransaction();
 
-            if (payer.getBalance() < amount) {
-                throw new Exception("O saldo do usuário é menor do que o valor a ser transferido");
-            }
+            this.concludeTransaction(payer, payee, amount);
 
+            transaction.commit();
+        }catch(Exception e){
+            if(transaction!=null){
+                transaction.rollback();
+            }
+            throw new Exception("Transaction service - " + e.getMessage());
+        }finally{
+            if(session != null){
+                session.close();
+            }
+        }
+    }
+    private void validatePayerTypeIsNotCustomer(User payer) throws Exception {
+        if (payer.getType() == UserType.SHOPKEEPER) {
+            throw new Exception("O pagador não pode ser lojista");
+        }
+    }
+
+    private void validatePayerHasEnoughCredits(User payer, Double amount) throws Exception {
+        if (payer.getBalance() < amount) {
+            throw new Exception("O saldo do usuário é menor do que o valor a ser transferido");
+        }
+    }
+
+    private void authorizeTransaction() throws Exception {
+        if (!this.authorizationGateway.authorizeTransaction()) {
+            throw new Exception("Transação bloqueada!");
+        }
+    }
+
+    private void concludeTransaction(User payer, User payee, Double amount) throws Exception {
+        try{
             payer.setBalance(payer.getBalance() - amount);
             payee.setBalance(payee.getBalance() + amount);
 
-            if (!this.authorizationGateway.authorizeTransaction()) {
-                throw new Exception("Transação bloqueada!");
-            }
             this.userService.save(payer);
             this.userService.save(payee);
 
             this.notificationSender.sendNotification(payee.getEmail(), payee.getName(), amount);
-        }catch(Exception e){
-            throw new Exception("Transaction service - " + e.getMessage());
+        }catch (Exception e){
+            throw new Exception("Ocoreu um problema durante a conclusão da transação: " + e.getMessage());
         }
     }
 }
